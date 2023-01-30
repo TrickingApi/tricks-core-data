a11a921f75b59aaf8d22c7a34008153c7a7962cc
Mikael Mantis <mikael.mantis7@gmail.com>
2023-01-29 23:25:10 -0800

    renames json files to camelCase

--- json_parser.py
+++ json_parser.py
@@ -5,7 +5,7 @@ f = open('./data/tricks.json')
 allTricks = json.load(f)
 
 for i in allTricks:
-  newFileName = "./tricks/" + i["name"] + ".json"
+  newFileName = "./tricks/" + i["id"] + ".json"
   with open(newFileName, "w") as outfile:
     json.dump(i, outfile)
   
